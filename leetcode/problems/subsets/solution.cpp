06045f92105dde9484faad95a486917f3ef0462d
Shubham Kumar <shubham2468sky@gmail.com>
2024-01-19 05:13:22 +0000

    Sync LeetCode submission - Subsets (cpp)

--- leetcode/problems/subsets/solution.cpp
+++ leetcode/problems/subsets/solution.cpp
@@ -1,30 +1,24 @@
 class Solution {
-private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-        //base case
-        if(index >= nums.size()) {
-            ans.push_back(output);
-            return ;
-        }
-        
-        //exclude
-        solve(nums, output, index+1, ans);
-        
-        //include
-        int element = nums[index];
-        output.push_back(element);
-        solve(nums, output, index+1, ans);
-        
-    }
-    
 public:
+    void solve( vector<vector<int>> &ans,vector<int>& nums,vector<int> output,int index){
+        if(index>=nums.size()){
+        ans.push_back(output);
+        return;
+        }
+
+        solve(ans,nums,output,index+1);
+        output.push_back(nums[index]);
+        solve(ans,nums,output,index+1);
+        // output.pop_back();
+
+
+    }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int> > ans;
-        vector<int> output;
-        int index = 0;
-        solve(nums, output, index, ans);
-        return ans;
-        
+       vector<vector<int>> ans;
+       vector<int> output;
+       int index=0;
+       solve(ans,nums,output,index);
+       return ans; 
     }
 };
