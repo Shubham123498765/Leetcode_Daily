3bfe14724f352ee83942bd9ba3418c597f522439
Shubham Kumar <shubham2468sky@gmail.com>
2024-02-25 13:34:47 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- leetcode/problems/climbing_stairs/solution.cpp
+++ leetcode/problems/climbing_stairs/solution.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int climbStairs(int n, unordered_map<int, int>& memo) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        if (memo.find(n) == memo.end()) {
-            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
-        }
-        return memo[n];
+    int helper(int n,vector<int>& dp){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int l=helper(n-1,dp);
+        int r=helper(n-2,dp);
+        return dp[n]=l+r;
     }
-
     int climbStairs(int n) {
-        unordered_map<int, int> memo;
-        return climbStairs(n, memo);
+        vector<int> dp(n+1,-1);
+        int prev=1;
+        int prev2=1;
+        int curr=1;
+        for(int i=2;i<=n;i++){
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return curr;
     }
 };
