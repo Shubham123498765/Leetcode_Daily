7c5e69b12b04c49448abf2ab1e028d4d5a6d1863
Shubham Kumar <shubham2468sky@gmail.com>
2024-05-24 04:34:03 +0000

    Sync LeetCode submission Runtime - 41 ms (38.22%), Memory - 51 MB (13.37%)

--- leetcode/0322-coin-change/solution.cpp
+++ leetcode/0322-coin-change/solution.cpp
@@ -17,13 +17,31 @@ public:
        }
 
     
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int sum) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans= helper(coins,amount,n,dp);
-        if(ans==1e9)
+        vector<vector<int>>dp(n+1,vector<int>(sum+1));
+        // int ans= helper(coins,amount,n,dp);
+        // if(ans==1e9)
+        // return -1;
+        // else 
+        // return ans;
+        // if (sum == 0) return 0; // If the sum is zero, no coins are needed
+        // if (n == 0) return 1e9;
+        for(int i=0;i<sum+1;i++) dp[0][i]=1e9;
+        for(int i=0;i<n+1;i++) dp[i][0]=0;
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(coins[i-1]<=j)
+                dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        if(dp[n][sum]>=1e9)
         return -1;
-        else 
-        return ans;
+        else
+        return dp[n][sum];
+
     }
 };
