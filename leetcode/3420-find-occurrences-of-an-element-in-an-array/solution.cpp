85cdc76f504428af44415393c7ff6caa1e1fc19a
Shubham Kumar <shubham2468sky@gmail.com>
2024-06-05 15:50:44 +0000

    Sync LeetCode submission Runtime - 162 ms (80.21%), Memory - 183.7 MB (83.95%)

--- leetcode/3420-find-occurrences-of-an-element-in-an-array/solution.cpp
+++ leetcode/3420-find-occurrences-of-an-element-in-an-array/solution.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-        for(int i=0;i<nums.size();i++)
-            if(nums[i]!=x)
-                nums[i]=0;
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries,
+                                     int x) {
+        for (auto& i : nums) {
+            if (i != x)
+                i = 0;
+        }
+        // for(int i=0;i<nums.size();i++)
+        //     if(nums[i]!=x)
+        //         nums[i]=0;
 
         // for(int i:nums)
         //     cout<<i<<" ";
-        
+
         vector<int> v;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0)
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0)
                 v.push_back(i);
         }
         vector<int> ans;
-        for(int i=0;i<queries.size();i++){
-            if(queries[i]>v.size())
+        for (int i = 0; i < queries.size(); i++) {
+            if (queries[i] > v.size())
                 ans.push_back(-1);
-            else 
-                ans.push_back(v[queries[i]-1]);
+            else
+                ans.push_back(v[queries[i] - 1]);
         }
         return ans;
     }
