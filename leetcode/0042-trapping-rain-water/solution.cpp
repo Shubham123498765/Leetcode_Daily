75728192ebf6b423b7232b57b1e6207fa8c37d63
Shubham Kumar <shubham2468sky@gmail.com>
2024-06-26 11:35:27 +0000

    Sync LeetCode submission Runtime - 12 ms (41.36%), Memory - 22.3 MB (62.81%)

--- leetcode/0042-trapping-rain-water/solution.cpp
+++ leetcode/0042-trapping-rain-water/solution.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if (n == 0) return 0;
-        
-        vector<int> left(n);
-        vector<int> right(n);
-        
-        // Fill left array
-        left[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = max(left[i - 1], height[i]);
-        }
-        
-        // Fill right array
-        right[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = max(right[i + 1], height[i]);
-        }
-        
-        // Calculate trapped water
-        int trappedWater = 0;
+        if (n == 0)
+            return 0;
+
+        int water = 0;
+        int max_height = INT_MIN;
+        int max_ind = 0;
+        int leftmax = 0, rightmax = 0;
         for (int i = 0; i < n; i++) {
-            trappedWater += min(left[i], right[i]) - height[i];
+            // max_height=max(max_height,height[i]);
+            if (max_height < height[i]) {
+                max_ind = i;
+                max_height = height[i];
+            }
         }
-        
-        return trappedWater;
+        for (int i = 0; i < max_ind; i++) {
+            leftmax = max(leftmax, height[i]);
+            water = water + min(max_height, leftmax) - height[i];
+        }
+        for (int i = n - 1; i > max_ind; i--) {
+            rightmax = max(rightmax, height[i]);
+            water = water + min(max_height, rightmax) - height[i];
+        }
+
+        return water;
     }
 };
