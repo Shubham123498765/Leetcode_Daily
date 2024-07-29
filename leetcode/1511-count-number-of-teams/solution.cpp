7a78d0c055415db8aeefc67c3afe87e4d21ec608
Shubham Kumar <shubham2468sky@gmail.com>
2024-07-29 12:26:32 +0000

    Sync LeetCode submission Runtime - 2613 ms (10.45%), Memory - 11.6 MB (52.64%)

--- leetcode/1511-count-number-of-teams/solution.cpp
+++ leetcode/1511-count-number-of-teams/solution.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
-   int numTeams(vector<int>& rating) {
-    int res = 0;
-    for (auto i = 1; i < rating.size() - 1; ++i) {
-        int less[2] = {}, greater[2] = {};
-        for (auto j = 0; j < rating.size(); ++j) {
-            if (rating[i] < rating[j])
-                ++less[j > i];
-            if (rating[i] > rating[j])
-                ++greater[j > i];
-        }
-        res += less[0] * greater[1] + greater[0] * less[1];
+    int ans=0;
+    void check(vector<int>& per){
+        int i=0;
+        if(per[i]<per[i+1] && per[i+1]<per[i+2])
+            ans++;
+        if(per[i]>per[i+1] && per[i+1]>per[i+2])
+            ans++;
     }
-    return res;
-}
+    bool check2(int a,int b,int c){
+        if((a<b && b<c) || (a>b && b>c))
+            return true;
+        return false; 
+    }
+
+
+    void generate(vector<int>& rating,vector<int>& per,int i,int n){
+        if(per.size()==3){
+            check(per);
+            return;
+        }
+        if(i>=n) return;
+        per.push_back(rating[i]);
+        generate(rating,per,i+1,n);
+        per.pop_back();
+        generate(rating,per,i+1,n);
+    }
+    int numTeams(vector<int>& rating) {
+        int n=rating.size();
+        // vector<int> per;
+        // generate(rating,per,0,n);
+        // return ans;
+        int ans1=0;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    if(check2(rating[i],rating[j],rating[k]))
+                        ans1++;
+                }
+            }
+        }
+        return ans1;
+    }
 };
