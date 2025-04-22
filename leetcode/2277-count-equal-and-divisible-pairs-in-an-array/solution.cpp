a94ff020e818c5ef494688996b7e73ab3f0ae928
Shubham Kumar <shubham2468sky@gmail.com>
2025-04-22 08:15:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.4 MB (69.46%)

--- leetcode/2277-count-equal-and-divisible-pairs-in-an-array/solution.cpp
+++ leetcode/2277-count-equal-and-divisible-pairs-in-an-array/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-   int countPairs(vector<int>& nums, int k) {
-    int res = 0;
-    unordered_map<int, vector<int>> m;
-    for (int i = 0;  i < nums.size(); ++i)
-        m[nums[i]].push_back(i);
-    for (auto &[n, ids] : m) {
-        unordered_map<int, int> gcds;
-        for (auto i : ids) {
-            auto gcd_i = gcd(i, k);
-            for (auto &[gcd_j, cnt] : gcds)
-                res += gcd_i * gcd_j % k ? 0 : cnt;
-            ++gcds[gcd_i];
+    int countPairs(vector<int>& nums, int k) {
+        int cnt=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j] && (i*j)%k==0)
+                cnt++;
+            }
         }
+        return cnt;
     }
-    return res;
-}
 };
